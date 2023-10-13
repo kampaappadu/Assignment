59603fa64a6fe3da3cd02ae3e2a90a01d1b2714b
kampaappadu <147789477+kampaappadu@users.noreply.github.com>
2023-10-13 13:00:20 +0530

    RomanToInteger by using Java

---
+++ RomanToInteger.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class RomanToInteger {
+    public static void main(String[] args) {
+        String romanNumber = "IX";
+        int result = romanToInt(romanNumber);
+        System.out.println("Roman numeral "+romanNumber+"\tis equilant to "+result);
+    }
+    public static  int romanToInt(String s){
+        Map<Character,Integer> romanValues = new HashMap<>();
+        romanValues.put('I',1);
+        romanValues.put('V',5);
+        romanValues.put('X',10);
+        romanValues.put('L',50);
+        romanValues.put('C',100);
+        romanValues.put('D',500);
+        romanValues.put('M',1000);
+        int result = 0;
+        int previousValue = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = romanValues.get(s.charAt(i));
+            if (currentValue < previousValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+            previousValue = currentValue;
+        }
+
+        return result;
+    }
+}
